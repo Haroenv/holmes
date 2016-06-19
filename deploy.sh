14ee2ac6a0ae2676a4883fe97bca4f4c66a21abe
Haroen Viaene <hello@haroen.me>
2016-06-19 05:52:20 +0200

    use deploy script

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# only when not on a pull request we deploy
+if [[ $TRAVIS_PULL_REQUEST == 'false' ]]; then
+  npm run doc
+  git config user.name "Haroen Viaene [bot]"
+  git config user.email "hello@haroen.me"
+  git commit -am "Built documentation [ci skip]"
+  git push https://${GH_OAUTH_TOKEN}@github.com/${GH_OWNER}/${GH_PROJECT_NAME} HEAD:gh-pages  > /dev/null 2>&1; fi
+fi
