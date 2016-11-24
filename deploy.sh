d0c98ca8f7ad825060abb8d2f45f716297d9a4f5
Haroen Viaene <hello@haroen.me>
2016-11-24 16:39:41 +0100

    coverage isn't tracked

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,6 @@ if [[ $TRAVIS_PULL_REQUEST == 'false' ]]; then
   npm run doc
   git config user.name "Haroen Viaene [bot]"
   git config user.email "hello@haroen.me"
-  npm run test
-  git commit -am "Build coverage & coverage [ci skip]"
+  git commit -am "Build documentation [ci skip]"
   git push https://${GH_OAUTH_TOKEN}@github.com/Haroenv/holmes HEAD:gh-pages > /dev/null 2>&1
 fi
