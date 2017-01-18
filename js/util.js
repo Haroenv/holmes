a266447ae85350b9e5de6578cf0cc145b5b21f0f
Haroen Viaene <hello@haroen.me>
2017-01-18 22:47:44 +0100

    indexOf is the same is includes basically
    simpler version of https://github.com/kevva/string-includes

--- js/util.js
+++ js/util.js
@@ -1,56 +1,5 @@
 const _global = typeof window !== 'undefined' ? window : global;
 
-/*! https://mths.be/includes v1.0.0 by @mathias */
-if (!String.prototype.includes) {
-  (function() {
-    'use strict'; // needed to support `apply`/`call` with `undefined`/`null`
-    var toString = {}.toString;
-    var defineProperty = (function() {
-      // IE 8 only supports `Object.defineProperty` on DOM elements
-      try {
-        var object = {};
-        var $defineProperty = Object.defineProperty;
-        var result = $defineProperty(object, object, object) && $defineProperty;
-      } catch(error) {}
-      return result;
-    }());
-    var indexOf = ''.indexOf;
-    var includes = function(search) {
-      if (this == null) {
-        throw TypeError();
-      }
-      var string = String(this);
-      if (search && toString.call(search) == '[object RegExp]') {
-        throw TypeError();
-      }
-      var stringLength = string.length;
-      var searchString = String(search);
-      var searchLength = searchString.length;
-      var position = arguments.length > 1 ? arguments[1] : undefined;
-      // `ToInteger`
-      var pos = position ? Number(position) : 0;
-      if (pos != pos) { // better `isNaN`
-        pos = 0;
-      }
-      var start = Math.min(Math.max(pos, 0), stringLength);
-      // Avoid the `indexOf` call if no match is possible
-      if (searchLength + start > stringLength) {
-        return false;
-      }
-      return indexOf.call(string, searchString, pos) != -1;
-    };
-    if (defineProperty) {
-      defineProperty(String.prototype, 'includes', {
-        'value': includes,
-        'configurable': true,
-        'writable': true
-      });
-    } else {
-      String.prototype.includes = includes;
-    }
-  }());
-}
-
 /**
  * makes sure that a class can be used as a function
  * @param  {class} Class    The class to transform
@@ -70,3 +19,7 @@ export function toFactory(Class) {
   Factory.prototype = Class.prototype
   return Factory
 }
+
+export const stringIncludes = (str: string, search: string) => {
+  return str.indexOf(search) !== -1;
+};
