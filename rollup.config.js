e17cb27ce16b7d3b5614e9da67f456f254d7ed16
Haroen Viaene <hello@haroen.me>
2017-09-07 10:19:44 +0200

    chore(deps): update dependencies
    newer rollup version gives slightly smaller code, will do patch release

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,16 @@
 import babel from 'rollup-plugin-babel';
 
 export default {
-  entry: 'js/main.js',
+  input: 'js/main.js',
   plugins: [
     babel({
       babelrc: false,
       presets: ['es2015-rollup', 'babili'],
-      plugins: ['transform-flow-strip-types']
-    })
+      plugins: ['transform-flow-strip-types'],
+    }),
   ],
-  targets: [
-    {dest: 'js/holmes.js', format: 'umd', moduleName: 'holmes'},
-    {dest: 'js/holmes.es.js', format: 'es'}
-  ]
+  output: [
+    { file: 'js/holmes.js', format: 'umd', name: 'holmes' },
+    { file: 'js/holmes.es.js', format: 'es' },
+  ],
 };
